28b761cc6f8fd0899822fc344e0c80ce891ef045
garciar316 <sergio980316@hotmail.com>
2022-06-17 16:18:14 -0500

    Se realiza la parte de react

--- react-consumer/src/root.component.tsx
+++ react-consumer/src/root.component.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import Formulario from "./components/Formulario.jsx";
+import PintarPersonajes from "./components/PintarPersonajes";
+
 export default function Root(props) {
-  return <section><h1>React consumer</h1></section>;
+  const [nombre, setNombre] = useState("");
+  return(
+    <div className="container">
+            <h1>APP</h1>
+            <Formulario setNombre={setNombre} />
+            <button
+                className="btn btn-success mt-2"
+                onClick={() => setNombre("")}
+            >
+                Reiniciar
+            </button>
+            <PintarPersonajes nombre={nombre} />
+        </div>
+  );
 }
